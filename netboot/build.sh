96ab484e89ef1b3bf6bbfa0a68b74eed7e699875
Yann Le Doaré <yann@linuxconsole.org>
2016-10-05 08:43:51 +0200

    http://jukebox.linuxconsole.org/sources/

--- netboot/build.sh
+++ netboot/build.sh
@@ -5,4 +5,12 @@ then
   cd rootfs
   cpio -idv < ../initramfs
 fi
+
+
+if [ ! -e rootfs/drivers ]
+then
+  mkdir rootfs/drivers
+  find /lib/modules/4.4.19/kernel/drivers/net/ -name *.ko | while read file; do cp $file rootfs/drivers ; done
+fi
+
 echo
