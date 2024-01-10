e6e0dfde4dfc57cb670d8494dd1fd36f38c45cca
Yann Le Doaré <yann@linuxconsole.org>
2024-01-10 08:39:35 +0000

    update steam launcher

--- addons/steam.sh
+++ addons/steam.sh
@@ -1,34 +1,20 @@
 cd $HOME
 
-if [ -e $HOME/.local/share/Steam/ ]
+if [ ! -e $HOME/.local/share/Steam/ ]
 then
-
-# sed -i "s@LD_LIBRARY_PATH@DISABLE_LINUXCONSOLE@g" $HOME/.local/share/Steam/steam.sh
-# echo ' ' > $HOME/.local/share/Steam/steamdeps.txt
-
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/.local/share/Steam/ubuntu12_32" 
-echo "DEBUG : $LD_LIBRARY_PATH" && sleep 5 
-export LIBGL_DRIVERS_PATH=/usr/lib32/dri/ 
-# $HOME/.local/share/Steam/steam.sh
-LD_LIBRARY_PATH="/home/yann/.local/share/Steam/ubuntu12_32/:/home/linuxconsole2019/multilib/lib:$LD_LIBRARY_PATH"  /home/yann/.local/share/Steam/ubuntu12_32/steam
-# /home/yann/.local/share/Steam/ubuntu12_32/steam
-
-else
 
 install -d src/steam
 cd src/steam
-[ ! -e steam-launcher ] && wget http://repo.steampowered.com/steam/pool/steam/s/steam/steam_1.0.0.62.tar.gz && tar xzvf steam_1.0.0.62.tar.gz
-# sed -i "s@id -u@echo@g" steam/steam
-# mkdir bootstrap
-# cd bootstrap
-# tar xJvf ../steam/bootstraplinux_ubuntu12_32.tar.xz
-#echo '
-#STEAM_RUNTIME=1
-#STEAM_DEPENDENCY_VERSION=1' > steamdeps.txt
-#sed -i "s@LD_LIBRARY_PATH@DISABLE_LINUXCONSOLE@g" steam.sh
-#tar cJvf ../steam/bootstraplinux_ubuntu12_32.tar.xz *
-# cd ..
-cd steam-launcher && DESTDIR=/tmp make install
-#LIBGL_DRIVERS_PATH=/usr/lib32/dri/ steam/steam
+[ ! -e steam-launcher ] && wget http://repo.steampowered.com/steam/pool/steam/s/steam/steam_1.0.0.78.tar.gz && tar xzvf steam_1.0.0.78.tar.gz
+cd steam-launcher && ./steam 
 
 fi
+
+export LD_LIBRARY_PATH="$HOME/.local/share/Steam/ubuntu12_32/:$HOME/src/ydfs/addons/lib:/usr/lib32:$HOME/.local/share/Steam/ubuntu12_32/steam-runtime/pinned_libs_32:$HOME/.local/share/Steam/ubuntu12_32/steam-runtime/usr/lib/i386-linux-gnu/:$HOME/.local/share/Steam/ubuntu12_32/steam-runtime/lib/i386-linux-gnu:$HOME/.local/share/Steam/ubuntu12_32/steam-runtime/usr/lib/i386-linux-gnu/gio/modules/:$LD_LIBRARY_PATH:$HOME/.local/share/Steam/ubuntu12_32" 
+echo "DEBUG : $LD_LIBRARY_PATH" && sleep 5 
+export LIBGL_DRIVERS_PATH=/usr/lib32/dri/ 
+export STEAM_RUNTIME=0
+export STEAM_RUNTIME_HEAVY=0
+export DBUS_FATAL_WARNINGS=0
+$HOME/.local/share/Steam/ubuntu12_32/steam
+
